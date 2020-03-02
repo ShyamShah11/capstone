7839a42544334416f9d7f65d58b6a513060a5060
Shyam Shah <shahs1@mcmaster.ca>
2020-03-02 01:08:58 -0500

    modified dimensions of input data and model

--- train.py
+++ train.py
@@ -13,17 +13,16 @@ np.set_printoptions(threshold=sys.maxsize)
 gestures={0:"palm", 1:"l-shape", 2:"fist", 3:"thumb", 4:"index", 5:"ok", 6:"c", 7:"none", 8:"none"}
 
 
-f=open("./data/white.txt","r")
-white_X = ast.literal_eval(f.read())
-white_X = np.delete(white_X, list(range(0, len(white_X[0]), 2)), axis=1)#reshape image
-print ("white_X", len(white_X), len(white_X[0]))
-f=open("./data/black.txt","r")
-black_X = ast.literal_eval(f.read())
-black_X = np.delete(black_X, list(range(0, len(black_X[0]), 2)), axis=1)#reshape image
-print ("black_X", len(black_X), len(black_X[0]))
+#f=open("./data/white.txt","r")
+#white_X = ast.literal_eval(f.read())
+#white_X = np.delete(white_X, list(range(0, len(white_X[0]), 2)), axis=1)#reshape image
+#print ("white_X", len(white_X), len(white_X[0]))
+#f=open("./data/black.txt","r")
+#black_X = ast.literal_eval(f.read())
+#black_X = np.delete(black_X, list(range(0, len(black_X[0]), 2)), axis=1)#reshape image
+#print ("black_X", len(black_X), len(black_X[0]))
 f=open("./data/palm.txt", "r")
 palm_X = ast.literal_eval(f.read())
-print ("palm_X", len(palm_X), len(palm_X[0]))
 f=open("./data/l.txt","r")
 l_X = ast.literal_eval(f.read())
 f=open("./data/fist.txt","r")
@@ -38,15 +37,15 @@ f=open("./data/c.txt","r")
 c_X = ast.literal_eval(f.read())
 
 
-X = np.concatenate((palm_X, l_X, fist_X, thumb_X, index_X, ok_X, c_X, white_X, black_X)) #create array with all image data
+X = np.concatenate((palm_X, l_X, fist_X, thumb_X, index_X, ok_X, c_X)) #create array with all image data
 print(X.shape)
-y = np.concatenate((np.zeros(len(palm_X)) , np.ones(len(l_X)), np.ones(len(l_X))*2, np.ones(len(l_X))*3, np.ones(len(l_X))*4, np.ones(len(l_X))*5, np.ones(len(l_X))*6, np.ones(len(white_X))*7, np.ones(len(black_X))*8)) #create array with actual gestures
+y = np.concatenate((np.zeros(len(palm_X)) , np.ones(len(l_X)), np.ones(len(l_X))*2, np.ones(len(l_X))*3, np.ones(len(l_X))*4, np.ones(len(l_X))*5, np.ones(len(l_X))*6)) #create array with actual gestures
 nsamples, nx, ny = X.shape #reshape array to fit into knn model
 X_reshaped = X.reshape((nsamples, nx*ny)) 
 print(X_reshaped.shape)
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_reshaped, y, test_size = 0.2, random_state=4)
 
-knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=7) #create model with 2 gestures
+knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=3) #create model with 2 gestures
 knn.fit(X_train, y_train) #train model
 #y_pred = knn.predict(X_test)
 #print (sklearn.metrics.accuracy_score(y_test,y_pred)) #checks accuracy of model
