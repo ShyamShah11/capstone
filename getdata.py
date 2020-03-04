533572b24444239713ced6c2571f61a626d4cafa
Shyam Shah <shahs1@mcmaster.ca>
2020-03-04 03:22:24 -0500

    small changes to tf model

--- getdata.py
+++ getdata.py
@@ -36,7 +36,7 @@ def convert_to_data(directory_name, gesture, output):
 
 def create_white (output):
     f=open(output, "wb") #switch to "a" to append
-    thresh = 255 - np.zeros((total_files,240,640))
+    thresh = np.zeros((total_files,240,640))
     np.save(f, thresh)
     f.close()
     
