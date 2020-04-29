9539fb285e3b7c3ec572c56acd791cb5190ed0a7
mcphej1 <32282097+mcphej1@users.noreply.github.com>
2020-04-29 00:42:52 -0400

    Update recordShortcut.py

--- recordShortcut.py
+++ recordShortcut.py
@@ -2,35 +2,39 @@ import threading
 import mouse
 import keyboard
 import time
-from playShortcut import play_shortcut
+from pickle import dump
 
-#create empty lists for the sequences of mouse and keyboard inputs
-mouse_events = []
-keyboard_events = []
+def record_Shortcut(name):
+    #create empty lists for the sequences of mouse and keyboard inputs
+    mouse_events = []
+    keyboard_events = []
+    #waits for special keypress before starting the recording
+    print("Press the '*' key when you would like to start recording")
+    keyboard.wait('*')
+    mouse.hook(mouse_events.append)
 
-#Asks for a name to associate to the shortcut
-name = input("Enter Shortcut Name : ")
-#waits for special keypress before starting the recording
-keyboard.wait("page up")
-mouse.hook(mouse_events.append)
+    #waits until the same keypress to stop recording
+    keyboard_events = keyboard.record(until='*')
+    mouse.unhook(mouse_events.append)
 
-#waits until the same keypress to stop recording
-keyboard_events = keyboard.record(until="page up")
-mouse.unhook(mouse_events.append)
+    print("Recording done!")
 
+    #print statements for testing you can remove these
+    #-----------------------------------------------
+    print(str(mouse_events))
+    print(str(keyboard_events))
+    #-----------------------------------------------
+    
+    #takes sequence of key and mouse presses as well as the shortcut name and stores in respective files
+    nfile = open("name_shortcuts.txt",'a+')
+    nfile.write("%s\n" % (name))
+    nfile.close()
 
-#takes sequence of key and mouse presses as well as the shortcut name and stores in respective text files
-nfile = open("name_shortcuts.txt","a+")
-nfile.write("%s\n" % (name))
-nfile.close()
+    mfile = open("mouse_shortcuts.txt",'ab+')
+    dump(mouse_events,mfile)
+    mfile.close()
 
-mfile = open("mouse_shortcuts.txt","a+")
-mfile.write("%s\n" % str(mouse_events))
-mfile.close()
-
-kfile = open("keyboard_shortcuts.txt","a+")
-kfile.write("%s\n" % str(keyboard_events))
-kfile.close()
-
-#plays what was just recorded (for the purpose of testing)
-play_shortcut(mouse_events,keyboard_events)
+    kfile = open("keyboard_shortcuts.txt",'ab+')
+    dump(keyboard_events,kfile)
+    kfile.close()
+#record_Shortcut("Grindin'")
